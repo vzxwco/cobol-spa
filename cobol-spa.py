269d6a454125c1d1109c05953d8770c73ca8dd58
Philip Busch <vzxwco@gmail.com>
2016-07-07 22:08:12 +0000

    Basic skeleton w/ command line option parsing.

---
+++ cobol-spa.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+
+import fileinput
+import argparse
+import sys
+
+TYPE_CONTROL = 'control'
+TYPE_DEP = 'dep'
+
+
+def parse_control(infile):
+	"Parse control flow graph."
+	for line in infile:
+		print (line)
+
+def parse_dep(infile):
+	"Parse dependency graph."
+	for line in infile:
+		print (line)
+
+
+# Parse command line options
+parser = argparse.ArgumentParser(description='COBOL Static Program Analysis')
+parser.add_argument('-v', '--version', action='version', version='%(prog)s 0.2')
+parser.add_argument('-t', '--type', choices=[TYPE_CONTROL, TYPE_DEP], required=True, help='Static program analysis type')
+parser.add_argument('-i', '--infile', nargs='?', type=argparse.FileType('r'), default=sys.stdin, help='Input file (default:stdin)')
+parser.add_argument('-o', '--outfile', nargs='?', type=argparse.FileType('w'), default=sys.stdout, help='Output file (default:stdout)')
+args = parser.parse_args()
+
+# Run parser
+if args.type == TYPE_CONTROL:
+	json = parse_control(args.infile)
+elif args.type == TYPE_DEP:
+	json = parse_dep(args.infile)
+else:
+	pass
+
+# Print result
+print (json)
+
+
